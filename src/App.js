bd625ad34b7cd18778ce80a75d824e98587d52cf
Jian <8270876@gmail.com>
2021-12-20 07:48:59 +0100

    initial commit for SuperDashboard

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home"
+import UserList from "./pages/userList/UserList"
+import User from "./pages/user/User"
+import "./app.css"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Topbar/>
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,123 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 2000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 2780,
+    },
+    {
+      name: 'May',
+      "Active User": 1890,
+    },
+    {
+      name: 'Jun',
+      "Active User": 2390,
+    },
+    {
+      name: 'Jul',
+      "Active User": 3490,
+    },
+    {
+        name: 'Aug',
+        "Active User": 3000,
+      },
+      {
+        name: 'Sep',
+        "Active User": 3490,
+      },
+      {
+        name: 'Oct',
+        "Active User": 1910,
+      },
+      {
+        name: 'Nov',
+        "Active User": 4870,
+      },
+      {
+        name: 'Dec',
+        "Active User": 4450,
+      },
+  ];
+
+export const userListDataRows = [
+  { id: 1, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 2, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 3, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 4, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 5, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 6, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 7, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 8, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 9, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+  { id: 10, 
+      username: 'Jon Snow', 
+      avatar: 'https://mui.com/static/images/avatar/1.jpg', 
+      email:"jon@gmail.com",
+      status:"active",
+      transaction:"$120.00" 
+  },
+];
